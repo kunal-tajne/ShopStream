0a4ef5faf29ddcdd0e4c921a33c0d0a3fd32c8e9
Kunal Tajne <kunal.tajne96@gmail.com>
2024-04-11 10:51:56 -0400

    Changes in Product  filter

--- src/Data/womenCollection.js
+++ src/Data/womenCollection.js
@@ -6,6 +6,48 @@ export const womenCollection=[
         "brand": "H&M",
         "title": "Active Seamless Biker Shorts",
 
+    },
+    {
+        "imageUrl": "https://raw.githubusercontent.com/kunal-tajne/algostruck/main/Active%20Ruched%20Drawstring%20Shorts.jpg",
+        "productLink":"https://shopstream-by-kunaltajne.vercel.app/product/130",
+        "brand": "Zara",
+        "title": "Active Ruched Drawstring Shorts",
+
+    },
+    {
+        "imageUrl": "https://raw.githubusercontent.com/kunal-tajne/algostruck/main/src/main/resources/Oscar%20the%20Grouch%20Graphic%20Tee.jpeg",
+        "productLink":"https://shopstream-by-kunaltajne.vercel.app/product/122",
+        "brand": "Forever 21",
+        "title": "Oscar the Grouch Graphic Tee",
+
+    },
+    {
+        "imageUrl": "https://raw.githubusercontent.com/kunal-tajne/ShopStream/main/src/Data/newArrivalsData/NewArrival3.webp",
+        "productLink":"https://shopstream-by-kunaltajne.vercel.app/product/115",
+        "brand": "Lacausa",
+        "title": "Plus Size Metallic Jacquard Maxi Dress",
+
+    },
+    {
+        "imageUrl": "https://raw.githubusercontent.com/kunal-tajne/algostruck/main/Sesame%20Street%20Graphic%20Pullover.jpg",
+        "productLink":"https://shopstream-by-kunaltajne.vercel.app/product/124",
+        "brand": "Gucci",
+        "title": "Sesame Street Graphic Pullover",
+
+    },
+    {
+        "imageUrl": "https://raw.githubusercontent.com/kunal-tajne/ShopStream/main/src/Data/newArrivalsData/NewArrival6.webp",
+        "productLink":"https://shopstream-by-kunaltajne.vercel.app/product/121",
+        "brand": "Esby",
+        "title": "Denim Raglan-Sleeve Bomber Jacket",
+
+    },
+    {
+        "imageUrl": "https://raw.githubusercontent.com/kunal-tajne/ShopStream/main/src/Data/newArrivalsData/NewArrival5.jpeg",
+        "productLink":"https://shopstream-by-kunaltajne.vercel.app/product/119",
+        "brand": "Harvest and Mill",
+        "title": "Double-Breasted Peak Lapel Blazer",
+
     }
 
 
